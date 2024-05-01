2cd9aeffdb1b939c80b37bafbf53f9adea4a5f78
Arvind Umrao <akumrao@yahoo.com>
2024-05-01 13:47:54 +0530

    tested rgba file with face detect event

--- src/facedetec/test/test.cpp
+++ src/facedetec/test/test.cpp
@@ -32,11 +32,12 @@
 
 #include <json/json.hpp>
 using json = nlohmann::json;
+#include "json/configuration.h"
 
 
 using namespace base;
 using namespace base::net;
-
+using namespace base::cnfg;
 void RestAPI(std::string method, std::string uri)
 {
     
@@ -69,6 +70,59 @@ void RestAPI(std::string method, std::string uri)
     conn->send();
     
 }
+#if XALIENT_TEST
+int XAProcess( uint8_t* buffer_containing_raw_rgb_data , int w, int h  )
+{
+
+    xa_fi_image_t image;
+    image.width = w;
+    image.height = h;
+
+    image.pixel_format =
+        XA_FI_COLOR_RGB888;  // signifies the buffer data format
+
+   // uint8_t* buffer_containing_raw_rgb_data = new uint8_t [image.width*3*image.height];
+
+    //memset(buffer_containing_raw_rgb_data, 0, image.width*3*image.height);
+
+
+    image.buff =  buffer_containing_raw_rgb_data;  // note this is in RGB order, otherwise
+                                         // colors will be swapped
+
+    xa_fi_error_t returnValue;
+
+    xa_sdk_process_image_outputs* process_image_outputs;
+
+    if (1) {
+        returnValue = xa_sdk_process_image(&image, &process_image_outputs);
+
+        if (returnValue == XA_ERR_NONE) {
+            for (int index = 0;
+                 index < process_image_outputs->number_of_json_blobs; ++index) {
+                xa_sdk_json_blob_t blob = process_image_outputs->blobs[index];
+
+                if (blob.blob_descriptor == XA_FACE_TRACK_EVENT) {
+                    //<
+                    // send blob -> json to Face Track Event endpoint >
+                    STrace << "json to Face Track Event endpoint: " <<  blob.json;
+
+                } else if (blob.blob_descriptor == XA_ACCURACY_MONITOR) {
+                    //<
+                    // send blob -> json to Accuracy Monitor endpoint >
+                    STrace << "send blob -> json to Accuracy Monitor endpoint: " <<  blob.json;
+                } else {
+                    SError << "Not a possible state";
+                }
+            }
+        } else {
+            SError << "Error at process_image_outputs";
+        }
+
+        //std::this_thread::sleep_for(std::chrono::milliseconds(500));
+    }
+
+}
+#endif
 
 
 int main(int argc, char** argv) {
@@ -86,6 +140,11 @@ int main(int argc, char** argv) {
        std::stringstream ss;
        ss << f.rdbuf(); // reading data
        str = ss.str();
+    }
+    else
+    {
+        SError << " rgba file does not exist ";
+        return -1;
     }
 
     
@@ -105,12 +164,68 @@ int main(int argc, char** argv) {
     
   unsigned char *  bgrBuf = rgba_to_rgb_brg( (const unsigned char*) str.c_str() , str.length(), bitmap_buffer_format_BGR , 0, width , height , &p_output_size );
    
+  //write_bmp(bgrBuf, width, height, "arvind.bmp"  );
+  
+    // test["arv"] = 1;
+
+#if XALIENT_TEST
+  
+
+    cnfg::Configuration config;
+
+    config.load("./config.json");
+
+    std::string xaconfig = config.root.dump();
+
+    xa_fi_error_t returnValue;
+
+    const char* path_to_vision_cell =
+        "/mnt/libxailient-fi-vcell.so";                    // For shared lib
+    returnValue = xa_sdk_initialize(path_to_vision_cell);  // For shared lib
+
+    // returnValue = xa_sdk_initialize(); // For static lib
+
+    if (returnValue != XA_ERR_NONE) {
+        SError << "Error at xa_sdk_initialize";
+
+        return -1;
+    }
+
+    const char* configuration = xaconfig.c_str();
+
+    STrace << "config json: " << configuration;
+
+    // xa_sdk_update_identities
+    // xa_sdk_update_identity_image
+    returnValue = xa_sdk_configure(configuration);
+    if (returnValue != XA_ERR_NONE) {
+        SError << "Error at xa_sdk_configure";
+
+        return -1;
+    }
+
+    returnValue = xa_sdk_is_face_recognition_enabled();
+    if (returnValue != XA_ERR_NONE) {
+        SError << "Error at xa_sdk_configure";
+
+        return -1;
+    }
+
+
+
            
-  write_bmp(bgrBuf, width, height, "arvind.bmp"  );
+
+    for( int x = 0; x < 100; ++x)
+    {
+        XAProcess( bgrBuf , width, height );
+
+        base::sleep(700);
+    }
+    
   
   
   free(bgrBuf) ;
-    
+  #endif  
     
   Application app;
 
@@ -170,38 +285,7 @@ int main(int argc, char** argv) {
 
 
    
-    json test = json::object();
 
-    // test["arv"] = 1;
-
-    std::string msg = "Hello, world!";
-
-    std::cout << msg << std::endl;
-
-#if XALIENT_TEST
-  
-
-    {
-
-        xa_fi_error_t returnValue;
-
-        const char * path_to_vision_cell = "/mnt/libxailient-fi-vcell.so"; // For shared lib
-        returnValue = xa_sdk_initialize(path_to_vision_cell); // For shared lib
-
-        // returnValue = xa_sdk_initialize(); // For static lib
-
-        if (returnValue != XA_ERR_NONE) {
-
-
-             SError << "xa_sdk_initialize failed ";
-
-        }
-
-
-
-    }
-
-#endif
 
 
 
