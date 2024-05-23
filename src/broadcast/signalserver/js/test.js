22e45651681ae74632b9ee8e4e83f798c413dbff
Arvind Umrao <akumrao@yahoo.com>
2024-05-23 17:10:20 +0530

    changed the json for identity

--- src/broadcast/signalserver/js/test.js
+++ src/broadcast/signalserver/js/test.js
@@ -438,3 +438,50 @@ function addIdentity()
 
   channelSnd.send(document.getElementById('w3review').value);
 }
+
+
+function identity(name)
+{
+  let uid = uuidV4();
+
+  var jsonStr = '{}';
+
+  var obj = {};
+  obj[uid]= {};
+  obj[uid]['accuracyMonitorConsent'] = false;
+  obj[uid]['identityName'] = name;
+  obj[uid]['productImprovementConsent'] = false;
+  obj[uid]['registrationImageIDs']=[];
+  obj[uid]['registrationImageIDs'].push(name);
+  
+  jsonStr = JSON.stringify(obj);
+
+  console.log(jsonStr);
+
+}
+
+identity("arvind");
+
+/*
+"76a92b24-31d5-463b-ab7a-b379efab7b30": {
+      "accuracyMonitorConsent": false,
+      "identityName": "arvind",
+      "productImprovementConsent": false,
+      "registrationImageIDs": [
+        "temp_new_identity_1_reg_image.jpg"
+      ]
+    },
+*/
+
+
+function uuidV4() {
+  const uuid = new Array(36);
+  for (let i = 0; i < 36; i++) {
+    uuid[i] = Math.floor(Math.random() * 16);
+  }
+  uuid[14] = 4; // set bits 12-15 of time-high-and-version to 0100
+  uuid[19] = uuid[19] &= ~(1 << 2); // set bit 6 of clock-seq-and-reserved to zero
+  uuid[19] = uuid[19] |= (1 << 3); // set bit 7 of clock-seq-and-reserved to one
+  uuid[8] = uuid[13] = uuid[18] = uuid[23] = '-';
+  return uuid.map((x) => x.toString(16)).join('');
+}
