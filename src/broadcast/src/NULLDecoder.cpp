9f425deaab0dc6849891cad006b737b473f212f7
Arvind Umrao <akumrao@yahoo.com>
2024-07-21 23:44:58 +0530

    rm storage folder with system command

--- src/broadcast/src/NULLDecoder.cpp
+++ src/broadcast/src/NULLDecoder.cpp
@@ -31,49 +31,49 @@ namespace base {
     namespace web_rtc {
 
         
-  int remove_directory(const char *path) {
-   DIR *d = opendir(path);
-   size_t path_len = strlen(path);
-   int r = -1;
-
-   if (d) {
-      struct dirent *p;
-
-      r = 0;
-      while (!r && (p=readdir(d))) {
-          int r2 = -1;
-          char *buf;
-          size_t len;
-
-          /* Skip the names "." and ".." as we don't want to recurse on them. */
-          if (!strcmp(p->d_name, ".") || !strcmp(p->d_name, ".."))
-             continue;
-
-          len = path_len + strlen(p->d_name) + 2; 
-          buf = malloc(len);
-
-          if (buf) {
-             struct stat statbuf;
-
-             snprintf(buf, len, "%s/%s", path, p->d_name);
-             if (!stat(buf, &statbuf)) {
-                if (S_ISDIR(statbuf.st_mode))
-                   r2 = remove_directory(buf);
-                else
-                   r2 = unlink(buf);
-             }
-             free(buf);
-          }
-          r = r2;
-      }
-      closedir(d);
-   }
-
-   if (!r)
-      r = rmdir(path);
-
-   return r;
-}
+//  int remove_directory(const char *path) {
+//   DIR *d = opendir(path);
+//   size_t path_len = strlen(path);
+//   int r = -1;
+//
+//   if (d) {
+//      struct dirent *p;
+//
+//      r = 0;
+//      while (!r && (p=readdir(d))) {
+//          int r2 = -1;
+//          char *buf;
+//          size_t len;
+//
+//          /* Skip the names "." and ".." as we don't want to recurse on them. */
+//          if (!strcmp(p->d_name, ".") || !strcmp(p->d_name, ".."))
+//             continue;
+//
+//          len = path_len + strlen(p->d_name) + 2; 
+//          buf = malloc(len);
+//
+//          if (buf) {
+//             struct stat statbuf;
+//
+//             snprintf(buf, len, "%s/%s", path, p->d_name);
+//             if (!stat(buf, &statbuf)) {
+//                if (S_ISDIR(statbuf.st_mode))
+//                   r2 = remove_directory(buf);
+//                else
+//                   r2 = unlink(buf);
+//             }
+//             free(buf);
+//          }
+//          r = r2;
+//      }
+//      closedir(d);
+//   }
+//
+//   if (!r)
+//      r = rmdir(path);
+//
+//   return r;
+//}
  /*
 static  unsigned char *find_start_code( unsigned char *h264_data, int h264_data_bytes, int *zcount)
 {
@@ -477,9 +477,12 @@ int parse_nal(  unsigned char **nal, int &length , int & payload_type, int &size
                         {
                             for (json::iterator itDT =  itDate.value().begin(); itDT != itDate.value().end(); ++itDT) 
                             {
-                                std::cout << *itDT << '\n';
-                                std::string tmp = Settings::configuration.storage +  itDT->get<std::string>() ;
-                                remove_directory(tmp.c_str());
+                                SInfo <<  "delete " << *itDT ;
+                                std::string tmp = "rm -rf " + Settings::configuration.storage +  itDT->get<std::string>() ;
+                                
+                                system(tmp.c_str());
+                                
+                                //remove_directory(tmp.c_str());
                             }
 
                             mf.root.erase(lwdates);
